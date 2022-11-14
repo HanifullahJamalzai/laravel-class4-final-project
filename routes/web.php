40daf7cec6ea3aae37912dca0050ea7db164d71e
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-11-14 14:50:38 +0430

    Route anme

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\admin\DashboardController::class, 'index']);
+Route::get('/', [App\Http\Controllers\admin\DashboardController::class, 'index'])->name('admin');

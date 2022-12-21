817eeadca6e172a8ce9f69963eb533d807dbdd0f
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-12-21 14:55:50 +0430

    pagination and filter by category

--- resources/views/landing/posts.blade.php
+++ resources/views/landing/posts.blade.php
@@ -8,102 +8,25 @@
             <div class="col-md-9">
               <h3 class="category-title">Search Results</h3>
   
+              @foreach ($posts as $post)
+                  
               <div class="d-md-flex post-entry-2 small-img">
                 <a href="single-post.html" class="me-4 thumbnail">
-                  <img src="assets/img/post-landscape-6.jpg" alt="" class="img-fluid">
+                  <img src="{{ $post->photo }}" alt="" class="img-fluid">
                 </a>
                 <div>
-                  <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-                  <h3><a href="single-post.html">What is the son of Football Coach John Gruden, Deuce Gruden doing Now?</a></h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas.</p>
-                  <div class="d-flex align-items-center author">
-                    <div class="photo"><img src="assets/img/person-2.jpg" alt="" class="img-fluid"></div>
-                    <div class="name">
-                      <h3 class="m-0 p-0">Wade Warren</h3>
-                    </div>
-                  </div>
+                  <div class="post-meta"><span class="date">{{ $post->category->name }}</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+                  <h3><a href="single-post.html">{{ $post->title }}</a></h3>
+                  <p>{!! str()->limit($post->description, 100, '...') !!}</p>
                 </div>
               </div>
+              
+              @endforeach
   
-              <div class="d-md-flex post-entry-2 small-img">
-                <a href="single-post.html" class="me-4 thumbnail">
-                  <img src="assets/img/post-landscape-1.jpg" alt="" class="img-fluid">
-                </a>
-                <div>
-                  <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-                  <h3><a href="single-post.html">17 Pictures of Medium Length Hair in Layers That Will Inspire Your New Haircut</a></h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas.</p>
-                  <div class="d-flex align-items-center author">
-                    <div class="photo"><img src="assets/img/person-2.jpg" alt="" class="img-fluid"></div>
-                    <div class="name">
-                      <h3 class="m-0 p-0">Wade Warren</h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  
-              <div class="d-md-flex post-entry-2 small-img">
-                <a href="single-post.html" class="me-4 thumbnail">
-                  <img src="assets/img/post-landscape-2.jpg" alt="" class="img-fluid">
-                </a>
-                <div>
-                  <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-                  <h3><a href="single-post.html">The Best Homemade Masks for Face (keep the Pimples Away)</a></h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas.</p>
-                  <div class="d-flex align-items-center author">
-                    <div class="photo"><img src="assets/img/person-2.jpg" alt="" class="img-fluid"></div>
-                    <div class="name">
-                      <h3 class="m-0 p-0">Wade Warren</h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  
-              <div class="d-md-flex post-entry-2 small-img">
-                <a href="single-post.html" class="me-4 thumbnail">
-                  <img src="assets/img/post-landscape-3.jpg" alt="" class="img-fluid">
-                </a>
-                <div>
-                  <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-                  <h3><a href="single-post.html">10 Life-Changing Hacks Every Working Mom Should Know</a></h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas.</p>
-                  <div class="d-flex align-items-center author">
-                    <div class="photo"><img src="assets/img/person-2.jpg" alt="" class="img-fluid"></div>
-                    <div class="name">
-                      <h3 class="m-0 p-0">Wade Warren</h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  
-              <div class="d-md-flex post-entry-2 small-img">
-                <a href="single-post.html" class="me-4 thumbnail">
-                  <img src="assets/img/post-landscape-4.jpg" alt="" class="img-fluid">
-                </a>
-                <div>
-                  <div class="post-meta"><span class="date">Business</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-                  <h3><a href="single-post.html">9 Half-up/half-down Hairstyles for Long and Medium Hair</a></h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas.</p>
-                  <div class="d-flex align-items-center author">
-                    <div class="photo"><img src="assets/img/person-2.jpg" alt="" class="img-fluid"></div>
-                    <div class="name">
-                      <h3 class="m-0 p-0">Wade Warren</h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
   
               <!-- Paging -->
               <div class="text-start py-4">
-                <div class="custom-pagination">
-                  <a href="#" class="prev">Prevous</a>
-                  <a href="#" class="active">1</a>
-                  <a href="#">2</a>
-                  <a href="#">3</a>
-                  <a href="#">4</a>
-                  <a href="#">5</a>
-                  <a href="#" class="next">Next</a>
-                </div>
+                {{ $posts->links() }}
               </div><!-- End Paging -->
   
             </div>
